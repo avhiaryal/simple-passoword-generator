1c55559300ab7008848f84e83c3d55ffe9bb1fd1
Avhi Aryal <avhiaryal54@gmail.com>
2020-07-07 23:50:19 +0545

    last

--- simple_password_generator.py
+++ simple_password_generator.py
@@ -1,22 +1,19 @@
-#follow @kapeed_thetechguy
+# follow @kapeed_thetechguy
 
 import string
 import random
 
-if __name__== "__main__":
+if __name__ == "__main__":
     l = string.ascii_lowercase
     u = string.ascii_uppercase
     d = string.digits
     p = string.punctuation
-
-
     passlen = int(input("Enter the length of your password: "))
     st = []
     st.extend(list(l))
     st.extend(list(u))
     st.extend(list(d))
     st.extend(list(p))
-    
+
     print("The Password is: ", end="")
     print("".join(random.sample(st, passlen)))
-    
